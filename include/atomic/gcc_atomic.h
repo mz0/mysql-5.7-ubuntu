e7e54d2378bb7b986417f2ac750bc66ced33a7a5
Robie Basak <robie.basak@canonical.com>
2016-07-15 03:15:14 +0200

    mysql-atomic.patch
    No DEP3 Subject or Description header found
    
    Gbp-Pq: mysql-atomic.patch.

--- include/atomic/gcc_atomic.h
+++ include/atomic/gcc_atomic.h
@@ -1,7 +1,7 @@
-#ifndef ATOMIC_GCC_BUILTINS_INCLUDED
-#define ATOMIC_GCC_BUILTINS_INCLUDED
+#ifndef GCC_ATOMIC_INCLUDED
+#define GCC_ATOMIC_INCLUDED
 
-/* Copyright (c) 2008, 2014, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -16,89 +16,79 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
+/* New GCC __atomic builtins introduced in GCC 4.7 */
+
 static inline int my_atomic_cas32(int32 volatile *a, int32 *cmp, int32 set)
 {
-  int32 cmp_val= *cmp;
-  int32 sav= __sync_val_compare_and_swap(a, cmp_val, set);
-  int ret= (sav == cmp_val);
-  if (!ret)
-    *cmp = sav;
-  return ret;
+  return __atomic_compare_exchange_n(a, cmp, set, 0,
+                                     __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
 }
 
 static inline int my_atomic_cas64(int64 volatile *a, int64 *cmp, int64 set)
 {
-  int64 cmp_val= *cmp;
-  int64 sav= __sync_val_compare_and_swap(a, cmp_val, set);
-  int ret= (sav == cmp_val);
-  if (!ret)
-    *cmp = sav;
-  return ret;
+  return __atomic_compare_exchange_n(a, cmp, set, 0,
+                                     __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
 }
 
 static inline int my_atomic_casptr(void * volatile *a, void **cmp, void *set)
 {
-  void *cmp_val= *cmp;
-  void *sav= __sync_val_compare_and_swap(a, cmp_val, set);
-  int ret= (sav == cmp_val);
-  if (!ret)
-    *cmp = sav;
-  return ret;
+  return __atomic_compare_exchange_n(a, cmp, set, 0,
+                                     __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
 }
 
 static inline int32 my_atomic_add32(int32 volatile *a, int32 v)
 {
-  return __sync_fetch_and_add(a, v);
+  return __atomic_fetch_add(a, v, __ATOMIC_SEQ_CST);
 }
 
 static inline int64 my_atomic_add64(int64 volatile *a, int64 v)
 {
-  return __sync_fetch_and_add(a, v);
+  return __atomic_fetch_add(a, v, __ATOMIC_SEQ_CST);
 }
 
 static inline int32 my_atomic_fas32(int32 volatile *a, int32 v)
 {
-  return __sync_lock_test_and_set(a, v);
+  return __atomic_exchange_n(a, v, __ATOMIC_SEQ_CST);
 }
 
 static inline int64 my_atomic_fas64(int64 volatile *a, int64 v)
 {
-  return __sync_lock_test_and_set(a, v);
+  return __atomic_exchange_n(a, v, __ATOMIC_SEQ_CST);
 }
 
 static inline void * my_atomic_fasptr(void * volatile *a, void * v)
 {
-  return __sync_lock_test_and_set(a, v);
+  return __atomic_exchange_n(a, v, __ATOMIC_SEQ_CST);
 }
 
 static inline int32 my_atomic_load32(int32 volatile *a)
 {
-  return __sync_fetch_and_or(a, 0);
+  return __atomic_load_n(a, __ATOMIC_SEQ_CST);
 }
 
 static inline int64 my_atomic_load64(int64 volatile *a)
 {
-  return __sync_fetch_and_or(a, 0);
+  return __atomic_load_n(a, __ATOMIC_SEQ_CST);
 }
 
 static inline void* my_atomic_loadptr(void * volatile *a)
 {
-  return __sync_fetch_and_or(a, 0);
+  return __atomic_load_n(a, __ATOMIC_SEQ_CST);
 }
 
 static inline void my_atomic_store32(int32 volatile *a, int32 v)
 {
-  (void) __sync_lock_test_and_set(a, v);
+  __atomic_store_n(a, v, __ATOMIC_SEQ_CST);
 }
 
 static inline void my_atomic_store64(int64 volatile *a, int64 v)
 {
-  (void) __sync_lock_test_and_set(a, v);
+  __atomic_store_n(a, v, __ATOMIC_SEQ_CST);
 }
 
 static inline void my_atomic_storeptr(void * volatile *a, void *v)
 {
-  (void) __sync_lock_test_and_set(a, v);
+  __atomic_store_n(a, v, __ATOMIC_SEQ_CST);
 }
 
-#endif /* ATOMIC_GCC_BUILTINS_INCLUDED */
+#endif /* GCC_ATOMIC_INCLUDED */
